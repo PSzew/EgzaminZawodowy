452ecede701528aee1b0ec2ede854e5627b7be20
Paweł Szewczyk <Pawel.Szewczyk@zstio.edu.pl>
2023-09-07 21:39:12 +0200

    Dodaj pliki projektów.

---
+++ EgzaminZawodowy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            bool Get()
+            {
+                Console.Write("Podaj a: ");
+                if(!Int32.TryParse(Console.ReadLine(),out a))
+                {
+                    return false;
+                }
+                Console.Clear();
+                Console.Write("Podaj b: ");
+                if (!Int32.TryParse(Console.ReadLine(), out b))
+                {
+                    return false;
+                }
+                return true;
+            }
+            int NWD(int A , int B)
+            {
+                while(A != B)
+                {
+                    if (A > B)
+                        A -= B;
+                    else
+                        B -= A;                   
+                }
+                return A;
+            }
+            if(Get())
+            {
+                Console.WriteLine("NWD: "+NWD(a,b)); 
+            }
+            else
+                Console.WriteLine("Podano nie prawidłowe dane!");
+            
+            
+        }
+    }
+}
